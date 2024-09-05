d64a5d4c761740a0eb3eefc006c1298ac5298d5b
luv <defendervdele@gmail.com>
2024-09-05 16:18:50 +0300

    gitignore & firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDmi7oHl2p-LvKiy87xyjujsECmZl0AaKAAAA',
-  authDomain: 'cloud-storage-7e445.firebaseapp.com',
-  projectId: 'cloud-storage-7e445',
-  storageBucket: 'cloud-storage-7e445.appspot.com',
-  messagingSenderId: '160322915280',
-  appId: '1:160322915280:web:9100cb7afae19439d5c7d5',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: 'cloud-storage-38b29.firebaseapp.com',
+  projectId: 'cloud-storage-38b29',
+  storageBucket: 'cloud-storage-38b29.appspot.com',
+  messagingSenderId: '289356711473',
+  appId: '1:289356711473:web:773cc00b94f67f2c64f710',
 }
 
 // Initialize Firebase
